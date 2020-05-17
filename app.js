96a42f40a99676d3fe2606b2155b789b93b8ce59
Anh Le <kunquan2345@gmail.com>
2020-05-17 01:27:36 -0700

    Start building some simple schema and resolver
    _ This time we build differently from the last time.
      We separate schema and resolver, plus the schema is a string and not an object
    _ Resolver go into rootValue property where each value is corresponding to a value in query and mutation

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-  res.send("hello world");
-  next();
-});
-app.listen(3000, () => console.log("Listen to port 3000"));
+app.use(
+  "/graphql",
+  graphqlHttp({
+    graphiql: true,
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => ["hello", "all night coding"],
+      createEvent: (args) => args.name,
+    },
+  })
+);
+app.listen(3000, () => console.log("Listen kto port 3000"));
