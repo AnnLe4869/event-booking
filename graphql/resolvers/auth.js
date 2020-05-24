aec54a5eded022e6cf1dd8f712801265f2d13ca6
Anh Le <kunquan2345@gmail.com>
2020-05-24 04:19:17 -0700

    Add log in authentication
    _ We will send token to client if the authentication is success
    _ Not implement restriction yet - mean that user can still do all query and mutation

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/user");
 
 module.exports = {
@@ -19,6 +20,25 @@ module.exports = {
     } catch (err) {
       console.error(err);
       throw err; // This is needed so that the error message is displayed in GraphQL client
+    }
+  },
+  login: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) throw new Error("Your credentials are invalid");
+
+      const passwordCheckResult = await bcrypt.compare(password, user.password);
+      if (!passwordCheckResult) throw new Error("Your credentials are invalid");
+
+      const token = jwt.sign(
+        { userId: user._id, email },
+        "asecretthatcannotbeshared",
+        { expiresIn: "1h" }
+      );
+      return { userId: user._id, token, tokenExpiration: 1 };
+    } catch (err) {
+      console.error(err);
+      throw err;
     }
   },
 };
--- graphql/schema/index.js
+++ graphql/schema/index.js
@@ -24,6 +24,12 @@ module.exports = buildSchema(`
           updatedAt: String!
         }
 
+        type AuthData {
+          userId: ID!,
+          token: String!,
+          tokenExpiration: Int! 
+        }
+
         input EventInput {
           title: String!,
           description: String!,
@@ -38,7 +44,8 @@ module.exports = buildSchema(`
 
         type RootQuery {
             events: [Event!]!,
-            bookings: [Booking!]!
+            bookings: [Booking!]!,
+            login(email: String!, password: String!): AuthData!
         }
         type RootMutation {
             createEvent(eventInput: EventInput): Event
