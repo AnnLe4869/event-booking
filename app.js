31d8ca78fc5108e36942cfadf68bb3e9dfc1d4c5
Anh Le <kunquan2345@gmail.com>
2020-05-25 19:20:02 -0700

    Send authentication data to back-end
    _ Connect front-end and back-end.
      Set header of the response in back-end so that it accept request from all front-end
      Otherwise, will meet CORS error due to different IP
    _ Send request to server using fetch()
      Do it without Apollo seem more work
    _ It take a while for response to come
      Apollo have a loading variable to show that we are waiting

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(isAuth);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method == "OPTIONS") res.sendStatus(200);
+  next();
+});
 
 app.use(
   "/graphql",
@@ -30,6 +38,6 @@ mongoose
     }
   )
   .then(() => {
-    app.listen(8000, () => console.log("Listen to port 3000"));
+    app.listen(8000, () => console.log("Listen to port 8000"));
   })
   .catch((err) => console.error(err));
--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,21 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
 
 export default function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const inputHandler = (e) => {
+    if (e.target.name === "email") setEmail(e.target.value);
+    if (e.target.name === "password") setPassword(e.target.value);
+  };
+  const switchModeHandler = () => {
+    setIsLogin(!isLogin);
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    let requestBody = {
+      query: `
+        query {
+            login(email: "${email}", password: "${password}"){
+              userId,
+              token,
+              tokenExpiration
+            }
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser (userInput: {
+              email: "${email}", 
+              password: "${password}"
+            }){
+              _id,
+              email
+            }
+          }
+        `,
+      };
+    }
+
+    try {
+      const response = await fetch("/graphql", {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const { data } = await response.json();
+      setEmail(null);
+      setPassword(null);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
-      <form action="/graphql" method="post" className="auth-form">
+      <form className="auth-form" onSubmit={submitHandler}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            onChange={inputHandler}
+            value={email}
+          />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={inputHandler}
+            value={password}
+          />
         </div>
         <div className="form-actions">
-          <button type="submit">Sign in</button>
-          <button type="button">Switch to sign up</button>
+          <button type="submit">Submit</button>
+          <button type="button" onClick={switchModeHandler}>
+            Switch to {isLogin ? "Sign up" : "Log in"}
+          </button>
         </div>
       </form>
     </>
