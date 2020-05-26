4c2f5514d3526d9924bcf3b077dee134341f826d
Anh Le <kunquan2345@gmail.com>
2020-05-26 03:01:49 -0700

    Add modal to enter the event we want to create
    _ Just the styling and no code attach yet
    _ The modal can close when we cancel

--- client/src/page/Event.js
+++ client/src/page/Event.js
@@ -1,7 +1,36 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import AuthContext from "../context/auth-context";
+import "./Event.css";
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
 
 export default function Event() {
-  const context = useContext(AuthContext);
-  return <h1>Event</h1>;
+  const [creating, setCreating] = useState(false);
+  const startCreatingEventHandler = () => {
+    setCreating(!creating);
+  };
+  const modalConfirmHandler = () => {};
+  const modalCancelHandler = () => setCreating(false);
+  return (
+    <div className="events-control">
+      {creating ? (
+        <>
+          <Backdrop></Backdrop>
+          <Modal
+            title="Add event"
+            canConfirm={true}
+            canCancel={true}
+            onCancel={modalCancelHandler}
+            onConfirm={modalConfirmHandler}
+          >
+            <p>Modal content</p>
+          </Modal>
+        </>
+      ) : null}
+      <p>Share your own event!</p>
+      <button className="btn" onClick={startCreatingEventHandler}>
+        Create event
+      </button>
+    </div>
+  );
 }
